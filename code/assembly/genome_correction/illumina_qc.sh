499b51794a2856c7b6573235ef1aacc56655022e
Wiktor Gustafsson <wigu2376@r34.uppmax.uu.se>
2018-04-19 15:30:39 +0200

    transcriptome qc

--- code/assembly/genome_correction/illumina_qc.sh
+++ code/assembly/genome_correction/illumina_qc.sh
@@ -7,4 +7,4 @@ module load bioinfo-tools
 module load FastQC
 
 #execute
-fastqc /home/wigu2376/Durian/data/trimmed_data/RNA_trimmed_data/*fastq
+fastqc /home/wigu2376/Durian/data/trimmed_data/RNA_trimmed_data/*fastq.gz
