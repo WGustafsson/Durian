63656a45193d425ba78a481b3dd6205097d80954
Wiktor Gustafsson <wiktgust@IBG-D0022.ibg.uu.se>
2018-04-25 16:35:01 +0200

    reference scaffold from paper, finished eval script

--- code/assembly/assembly_evaluation/assembly_eval.sh
+++ code/assembly/assembly_evaluation/assembly_eval.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+#navigate to quast location (on local BMC Linux system, change for running elsewhere)
 cd /home/wiktgust/quast-4.6.3
+
+python quast.py /home/wiktgust/Durian/analyses/assembly/assembly_evaluation/input/contigs.fasta -R \
+/home/wiktgust/Durian/analyses/assembly/assembly_evaluation/input/reference.fasta -o \
+/home/wiktgust/Durian/analyses/assembly/assembly_evaluation/output
 
