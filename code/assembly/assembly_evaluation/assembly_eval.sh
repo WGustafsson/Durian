0e251f0e15922d5c1766b47d38866a132fec8a6b
Wiktor Gustafsson <wigu2376@rackham3.uppmax.uu.se>
2018-04-26 12:54:03 +0200

    scripts for assembly evaluation and illumina alignment + results from QUAST

--- code/assembly/assembly_evaluation/assembly_eval.sh
+++ code/assembly/assembly_evaluation/assembly_eval.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-#navigate to quast location (on local BMC Linux system, change for running elsewhere)
-cd /home/wiktgust/quast-4.6.3
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 02:00:00
+#SBATCH -J assembly_eval_wiktor
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user wiktorg.95@gmail.com
 
-python quast.py /home/wiktgust/Durian/analyses/assembly/assembly_evaluation/input/contigs.fasta -R \
-/home/wiktgust/Durian/analyses/assembly/assembly_evaluation/input/reference.fasta -o \
-/home/wiktgust/Durian/analyses/assembly/assembly_evaluation/output
+#modules
+module load bioinfo-tools
+module load quast
 
+quast.py /home/wigu2376/Durian/analyses/assembly/assembly_evaluation/input/contigs.fasta -R \
+/home/wigu2376/Durian/analyses/assembly/assembly_evaluation/input/reference.fasta -o \
+/home/wigu2376/Durian/analyses/assembly/assembly_evaluation/output
--- code/assembly/genome_alignment/alignment_batch.sh
+++ code/assembly/genome_alignment/alignment_batch.sh
@@ -1,2 +1,21 @@
-#!/bin/batch -l
+#!/bin/bash -l
+
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 10:00:00
+#SBATCH -J illumina_pacbio_align_wiktor
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user wiktorg.95@gmail.com
+
+# modules
+module load bioinfo-tools
+module load bwa
+
+#execute
+bwa index -p durian /home/wigu2376/Durian/analyses/assembly/genome_assembly/durian_pacbio.contigs.fasta
+
+bwa mem -t 2 /home/wigu2376/Durian/analyses/assembly/genome_assembly/durian_pacbio.contigs.fasta \ 
+/home/wigu2376/illumina_data/SRR6058604_scaffold_10.1P.fastq /home/wigu2376/illumina_data/SRR6058604_scaffold_10.2P.fastq > \
+/home/wigu2376/Durian/analyses/assembly/genome_alignment/align.sam
 
