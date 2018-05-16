90c12c4ad6ee8804680983828e60a8209f03a46e
Wiktor Gustafsson <wigu2376@rackham1.uppmax.uu.se>
2018-05-16 10:24:08 +0200

    reads counting

---
+++ code/diff_exp/reads_counting/reads_count.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 24:00:00
+#SBATCH -J reads_count
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user wiktorg.95@gmail.com
+
+module load bioinfo-tools samtools htseq
+
+samtools sort -n -O BAM  -@ 4 -o /home/wigu2376/Durian/analyses/annotation/mapping/sorted_name.bam \
+/home/wigu2376/Durian/analyses/annotation/mapping/sorted_map.bam
+
+htseq-count -f bam -r name -i ID -t gene \
+/home/wigu2376/Durian/analyses/annotation/mapping/sorted_name.bam \
+/home/wigu2376/Durian/analyses/annotation/structural/NW_019167827%2E1.gff \
+> /home/wigu2376/Durian/analyses/diff_exp/reads_counting/counts.file
