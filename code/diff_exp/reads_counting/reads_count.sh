d7713389582585d677f89f8c9ae2824e57389e2e
Wiktor Gustafsson <wigu2376@rackham3.uppmax.uu.se>
2018-05-17 01:19:50 +0200

    functional annotation

--- code/diff_exp/reads_counting/reads_count.sh
+++ code/diff_exp/reads_counting/reads_count.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2018003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 24:00:00
+#SBATCH -t 5:00:00
 #SBATCH -J reads_count
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user wiktorg.95@gmail.com
@@ -13,7 +13,7 @@ module load bioinfo-tools samtools htseq
 samtools sort -n -O BAM  -@ 4 -o /home/wigu2376/Durian/analyses/annotation/mapping/sorted_name.bam \
 /home/wigu2376/Durian/analyses/annotation/mapping/sorted_map.bam
 
-htseq-count -f bam -r name -i ID -t gene \
+htseq-count -f bam -r name -i ID -s no -t gene \
 /home/wigu2376/Durian/analyses/annotation/mapping/sorted_name.bam \
 /home/wigu2376/Durian/analyses/annotation/structural/NW_019167827%2E1.gff \
 > /home/wigu2376/Durian/analyses/diff_exp/reads_counting/counts.file
