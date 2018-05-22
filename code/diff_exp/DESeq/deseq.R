cfad302d155cfc343c0c1c47b5195a671dc15ab2
Wiktor Gustafsson <wigu2376@rackham3.uppmax.uu.se>
2018-05-22 13:23:59 +0200

    .

--- code/diff_exp/DESeq/deseq.R
+++ code/diff_exp/DESeq/deseq.R
@@ -1,2 +1,15 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite("DESeq2")
+
+directory <- "/home/wigu2376/Durian/analyses/diff_exp/reads_counting/individual_samples"
+samples = list.files(directory) 
+
+files = vector()
+
+for (sample in samples) {
+	samp_dir = paste(directory, sample, sep="/")
+	file = list.files(samp_dir)
+	files = c(files)
+}
+print(files)
+
